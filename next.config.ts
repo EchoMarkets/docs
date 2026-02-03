57481339bda7f347bc22484d73ea112f9e02421d
Ivan Molto <ivanmolto@gmail.com>
2026-02-03 10:04:43 +0100

    chore: Remove Next.js configuration file as part of project restructuring

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import withMarkdoc from "@markdoc/next.js";
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "md", "ts", "tsx"],
-};
-
-export default withMarkdoc({ schemaPath: "./markdoc" })(nextConfig);
